a725ccbaab7063586754205e328af259be878c32
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-03-19 15:51:26 +0100

    Unit test fixes and invoice impl. started

---
+++ Models/InvoiceStatus.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+declare(strict_types=1);
+namespace Modules\Billing\Models;
+
+use phpOMS\Datatypes\Enum;
+
+/**
+ * Task type enum.
+ *
+ * @category   Tasks
+ * @package    Modules
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+abstract class InvoiceStatus extends Enum
+{
+    /* public */ const ACTIVE = 1;
+    /* public */ const ARCHIVED  = 2;
+    /* public */ const DELETED   = 3;
+}
