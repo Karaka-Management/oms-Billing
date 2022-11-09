a813e1677a3d34627bd396af295ab1f025d464d3
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-09 23:02:52 +0100

    reduce serialization of null models

--- Models/NullBillElement.php
+++ Models/NullBillElement.php
@@ -36,4 +36,12 @@ final class NullBillElement extends BillElement
         $this->id = $id;
         parent::__construct();
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
